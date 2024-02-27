7fc066647dd7029a9bd10d52b05f1ff72ff03f04
imi-tat0r <admin@imi-tat0r.net>
2024-02-27 16:52:58 +0100

    Some more Helpers

--- DiscordChat/Extensions/PlayerControllerExtensions.cs
+++ DiscordChat/Extensions/PlayerControllerExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
 
 namespace DiscordChat.Extensions;
 
@@ -7,5 +8,28 @@ public static class PlayerControllerExtensions
     internal static bool IsPlayer(this CCSPlayerController? player)
     {
         return player is { IsValid: true, IsHLTV: false, IsBot: false, UserId: not null, SteamID: >0 };
+    }
+
+    internal static string GetTeamString(this CCSPlayerController? player, bool abbreviate = false)
+    {
+        return (player?.Team ?? 0) switch
+        {
+            CsTeam.None => abbreviate ? "-" : "None",
+            CsTeam.CounterTerrorist => abbreviate ? "CT" : "Counter-Terrorist",
+            CsTeam.Terrorist => abbreviate ? "T" : "Terrorist",
+            CsTeam.Spectator => abbreviate ? "Spec" : "Spectator",
+            _ => ""
+        };
+    }
+
+    internal static char GetChatColor(this CCSPlayerController? player)
+    {
+        return (player?.Team ?? 0) switch
+        {
+            CsTeam.Terrorist => ChatColors.Orange,
+            CsTeam.CounterTerrorist => ChatColors.Blue,
+            CsTeam.Spectator => ChatColors.LightPurple,
+            _ => ChatColors.Default
+        };
     }
 }
