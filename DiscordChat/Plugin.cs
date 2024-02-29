8ebe325ea652790d82b1b61bee61f7e41ae0f003
imi-tat0r <admin@imi-tat0r.net>
2024-02-29 09:26:09 +0100

    Updated README.MD
    Increased Version

--- DiscordChat/Plugin.cs
+++ DiscordChat/Plugin.cs
@@ -17,7 +17,7 @@ namespace DiscordChat;
 public class DiscordChatSync : BasePlugin, IPluginConfig<DiscordChatSyncConfig>
 {
     public override string ModuleName => "CS2-DiscordChatSync";
-    public override string ModuleVersion => "1.1.0";
+    public override string ModuleVersion => "1.1.1";
     public override string ModuleAuthor => "imi-tat0r";
     public override string ModuleDescription => "Syncs chat messages from and to a discord channel.";
     public DiscordChatSyncConfig Config { get; set; } = new();
